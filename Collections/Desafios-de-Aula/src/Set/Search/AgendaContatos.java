e2b4449576ee97d88c77be777f699085e119bff4
Lucas Martins <127567616+LucasPMartins@users.noreply.github.com>
2025-03-18 14:47:21 -0300

    exercicios de desafio de Map

---
+++ Collections/Desafios-de-Aula/src/Set/Search/AgendaContatos.java
@@ -0,0 +1,48 @@
+package Set.Search;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class AgendaContatos {
+    private Set<Contato> contatosSet;
+
+    public AgendaContatos() {
+        this.contatosSet = new HashSet<>();
+    }
+
+    public void adicionarContato(String nome, int numero) {
+        contatosSet.add(new Contato(nome, numero));
+    }
+
+    public void exibirContatos() {
+        if (!contatosSet.isEmpty()) {
+            System.out.println(contatosSet);
+        } else {
+            System.out.println("O conjunto está vazio!");
+        }
+    }
+
+    public Set<Contato> pesquisarPorNome(String nome) {
+        Set<Contato> contatosPorNome = new HashSet<>();
+        for (Contato c : contatosSet) {
+            if (c.getNome().startsWith(nome)) {
+                contatosPorNome.add(c);
+            }
+        }
+        return contatosPorNome;
+
+    }
+
+    public Contato atualizarNumeroContato(String nome, int novoNumero) {
+        Contato contatoAtualizado = null;
+        for (Contato c : contatosSet) {
+            if (c.getNome().equalsIgnoreCase(nome)) {
+                c.setNumero(novoNumero);
+                contatoAtualizado = c;
+                break;
+            }
+        }
+        return contatoAtualizado;
+
+    }
+}
